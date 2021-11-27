e7d23e70e92fc996ed739ec0d17268d0ffedd944
hwanginkyeong <inke0831@naver.com>
2021-11-27 12:52:04 +0900

    2nd

--- src/com/javaex/practice/Ex13.java
+++ src/com/javaex/practice/Ex13.java
@@ -8,7 +8,17 @@ public class Ex13 {
 	
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자를 입력해 주세요.");
-		System.out.print("숫자");
+		System.out.print("숫자: ");
+		int num = sc.nextInt();
+		
+		double result1 =((num*num*num)-(9*num) +2);
+		double result2 = 7*num + 2;
+	
+		if(num<=0) {
+			System.out.print("계산결과는 "+ result1);
+		}else if(num>0) {
+			System.out.print("계산결과는 " + result2);
+		}System.out.println(" 입니다.");
 		
 		
 		
